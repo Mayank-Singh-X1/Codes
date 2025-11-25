e68f7fbcb35ff2db43661fd639e4103a2cb9e289
Mayank-Singh <mayankbrawler@gmail.com>
2025-11-25 21:27:55 +0530

    dsa continued advance recursions

---
+++ java/Advance_recursions.java
@@ -0,0 +1,20 @@
+public class Advance_recursions {
+    public static void printperm(String str,String permutation){
+
+        if(str.length()==0){
+            System.out.println(permutation);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++){
+            char curr=str.charAt(i);
+            String newstring = str.substring(0,i) + str.substring(i+1);
+            printperm(newstring, permutation+curr);
+        }
+    }
+
+    public static void main(String[] args){
+        String str="abc";
+        printperm(str, "");
+    }
+}
