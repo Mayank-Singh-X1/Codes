bc54d59b30c3edba31ee5c910918c3c320b851a9
Mayank-Singh <mayankbrawler@gmail.com>
2025-11-30 16:40:00 +0530

    Functional programming

--- Python/functions.py
+++ Python/functions.py
@@ -1,2 +1,19 @@
-def isprime(n):
+# def add(*args):
+#     total=0
+#     for arg in args:
+#         total+=arg
+#     return total
+
+# print(add(1,2,3,4))
+
+def address(*args,**kwargs):
+    for a in args:
+        print(a)
+
+    print(f"{kwargs.get('street')}")
+    for key, value in kwargs.items():
+        print(f"hell yeah {key} : {value}")
+
+address(123,"jfjds",
+    street="hello",myname="maikiee" )
     
