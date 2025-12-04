787f11d3ab98f27f5f481fee64150b9375a9dc80
Mayank-Singh <mayankbrawler@gmail.com>
2025-12-04 22:28:38 +0530

    created first webpage using flask

---
+++ Flask/wepage.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return "This is home page"
+
+@app.route("/products")
+def products():
+    return "This is product page"
+
+if __name__=="__main__":
+    app.run(debug=True, port=8000)
