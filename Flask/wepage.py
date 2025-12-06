f0ccc597022c1a48e786330d7d2629d75c99634b
Mayank-Singh <mayankbrawler@gmail.com>
2025-12-06 15:41:53 +0530

    initialized databse in  flask

--- Flask/wepage.py
+++ Flask/wepage.py
@@ -1,6 +1,9 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app=Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"]="sqllite///:site.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATION_"]=False
 
 @app.route("/")
 def homepage():
