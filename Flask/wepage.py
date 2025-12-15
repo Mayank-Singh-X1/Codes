5247ee4e57a8214501607bb7c62ce2a83a66b70d
Mayank-Singh <mayankbrawler@gmail.com>
2025-12-15 16:20:20 +0530

    final push

--- Flask/wepage.py
+++ Flask/wepage.py
@@ -70,4 +70,4 @@ if __name__ == "__main__":
     # Create DB tables if they don't exist
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
