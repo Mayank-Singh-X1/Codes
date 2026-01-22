72a96ecb9e3d1aab823845f901d09985a0cc2a97
Mayank-Singh <mayankbrawler@gmail.com>
2026-01-22 11:28:41 +0530

    webdev

---
+++ CC03/JAVA/pattern.java
@@ -0,0 +1,12 @@
+package JAVA;
+
+public class pattern {
+    public static void main(String[] args) {
+        int n = 7;
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+        }
+
+    }
+
+}
